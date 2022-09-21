728bdf0ac3aa8b2e4ff496c6703d12c42877ee39
stalugaexpert <stalegapawel@gmail.com>
2022-09-21 09:13:28 +0200

    Wrapped pages in Layout component, cleaning styles.

--- pages/create.tsx
+++ pages/create.tsx
@@ -1,5 +1,6 @@
 import { createPost } from '@lib/firebase'
 import styles from '@styles/create.module.scss'
+import { Layout } from 'components'
 import { useRouter } from 'next/router'
 import { useState } from 'react'
 
@@ -53,60 +54,62 @@ const CreatePage = (): JSX.Element => {
   }
   
   return (
-    <div className={styles.CreatePage}>
-      <form onSubmit={handleSubmit}>
-        <h1>Create a new post</h1>
-        <div>
-          <label htmlFor="title">Title</label>
-          <input
-            id="title"
-            type="text"
-            value={formValues.title}
-            onChange={handleChange}
-          />
-        </div>
-        <div>
-          <label htmlFor="slug">Slug</label>
-          <input
-            id="slug"
-            type="text"
-            value={formValues.slug}
-            onChange={handleChange}
-          />
-        </div>
-        <div>
-          <label htmlFor="coverImage">Cover Image URL</label>
-          <input
-            id="coverImage"
-            type="text"
-            value={formValues.coverImage}
-            onChange={handleChange}
-          />
-        </div>
-        <div>
-          <label htmlFor="coverImageAlt">Cover Image Alt</label>
-          <input
-            id="coverImageAlt"
-            type="text"
-            value={formValues.coverImageAlt}
-            onChange={handleChange}
-          />
-        </div>
-        <div>
-          <label htmlFor="content">Content</label>
-          <textarea
-            id="content"
-            value={formValues.content}
-            onChange={handleChange}
-          />
-        </div>
-        <button
-          disabled={isLoading}
-          type="submit">
-          {isLoading ? 'Creating...' : 'Create'}
-        </button>
-      </form>
-    </div>
+    <Layout>
+      <div className={styles.CreatePage}>
+        <form onSubmit={handleSubmit}>
+          <h1>Create a new post</h1>
+          <div>
+            <label htmlFor="title">Title</label>
+            <input
+              id="title"
+              type="text"
+              value={formValues.title}
+              onChange={handleChange}
+            />
+          </div>
+          <div>
+            <label htmlFor="slug">Slug</label>
+            <input
+              id="slug"
+              type="text"
+              value={formValues.slug}
+              onChange={handleChange}
+            />
+          </div>
+          <div>
+            <label htmlFor="coverImage">Cover Image URL</label>
+            <input
+              id="coverImage"
+              type="text"
+              value={formValues.coverImage}
+              onChange={handleChange}
+            />
+          </div>
+          <div>
+            <label htmlFor="coverImageAlt">Cover Image Alt</label>
+            <input
+              id="coverImageAlt"
+              type="text"
+              value={formValues.coverImageAlt}
+              onChange={handleChange}
+            />
+          </div>
+          <div>
+            <label htmlFor="content">Content</label>
+            <textarea
+              id="content"
+              value={formValues.content}
+              onChange={handleChange}
+            />
+          </div>
+          <button
+            disabled={isLoading}
+            type="submit">
+            {isLoading ? 'Creating...' : 'Create'}
+          </button>
+        </form>
+      </div>
+    </Layout>
   )
 }
 
