9757b62e497ca970fdb5345bddb1b1f4c3bfd045
stalugaexpert <stalegapawel@gmail.com>
2022-09-21 09:20:19 +0200

    Added redirecting to 404 page if post doesn't exist.

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -2,9 +2,21 @@ import { getPostBySlug } from '@lib/firebase'
 import { getFormattedDate } from '@lib/utils'
 import styles from '@styles/post.module.scss'
 import { Layout } from 'components'
+import { useRouter } from 'next/router'
 import { BlogPost } from 'types/types'
 
-const PostPage = ({ post }: BlogPost): JSX.Element => {
+const PostPage = ({ post }: BlogPost): JSX.Element | undefined | null => {
+  const router = useRouter()
+
+  if (!post && typeof window !== 'undefined') {
+    router.push('/404')
+    return
+  }
+
+  if (!post) {
+    return null
+  }
+
   return (
     <Layout>
       <div className={styles.PostPage}>
